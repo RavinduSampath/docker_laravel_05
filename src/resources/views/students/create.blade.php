4259882701acbb14dddc6cee24f5c40a5caa34ad
Ravindu Sampath <praneethravindu50@gmail.com>
2025-08-04 17:14:50 +0530

    #02

---
+++ src/resources/views/students/create.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Student</title>
+</head>
+
+<body>
+    <h1>Add Student</h1>
+    <form action="{{ route('students.store') }}" method="POST">
+        @csrf
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div>
+            <label for="phone">Phone:</label>
+            <input type="text" id="phone" name="phone">
+        </div>
+        <div>
+            <label for="date_of_birth">Date of Birth:</label>
+            <input type="date" id="date_of_birth" name="date_of_birth">
+        </div>
+        <div>
+            <label for="address">Address:</label>
+            <textarea id="address" name="address"></textarea>
+        </div>
+        <button type="submit">Add Student</button>
+    </form>
+</body>
+
+</html>
